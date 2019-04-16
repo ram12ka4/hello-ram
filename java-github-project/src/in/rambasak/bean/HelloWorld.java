7e4a3f7c969136f62dc12601f78173f6152a920a
Ram Kumar Basak <ram.kumar.basak@gmail.com>
2019-04-16 16:02:57 +0530

    first commit into master

---
+++ java-github-project/src/in/rambasak/bean/HelloWorld.java
@@ -0,0 +1,10 @@
+package in.rambasak.bean;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
