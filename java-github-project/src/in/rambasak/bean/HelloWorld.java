161f6770a0c2bf937f69cce15b856df94d5c9368
Ram Kumar Basak <ram.kumar.basak@gmail.com>
2019-04-16 17:58:28 +0530

    commit from krishna 2nd time

--- java-github-project/src/in/rambasak/bean/HelloWorld.java
+++ java-github-project/src/in/rambasak/bean/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 		System.out.println("Hello World");
 		System.out.println("Hello World from krishna");
 		System.out.println("Hello World from sanjay");
+		System.out.println("Hello World from sanjay1");
 	}
 
 }
