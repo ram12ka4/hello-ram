100ac297769aa3ab3336ab0adb954292468e6178
Ram Kumar Basak <ram.kumar.basak@gmail.com>
2019-04-17 14:10:07 +0530

    helloworld class created in master package from krishna branch

---
+++ java-github-project/src/in/master/bean/HelloWorld.java
@@ -0,0 +1,10 @@
+package in.master.bean;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World from krishna");
+	}
+
+}
